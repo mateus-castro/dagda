5ac89ee8399d2c645988889cb1a5d61bbb4a54e3
mario-heleno <mario.silvafilho@bandtec.com.br>
2020-04-26 21:53:10 -0300

    Atualização pagina serviços

--- js/script.js
+++ js/script.js
@@ -5,3 +5,11 @@ function enter() {
 function fechar() {
     login_box.style.display = 'none';
 }
+
+function calcular_custo() {
+    var calculo = Number(numero_trafos.value) * 1399.99;
+    valor.innerHTML = calculo.toFixed(2);
+    plano_recomendado.innerHTML = 'Plano Avaliado';
+    var calculo_mensal = calculo / 12;
+    valor.innerHTML = calculo_mensal.toFixed(2);
+}
